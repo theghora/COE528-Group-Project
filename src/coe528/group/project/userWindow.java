b65df01f38ee621fcce7ac641d872dab85dbd084
ayeshhaaa <127570151+ayeshhaaa@users.noreply.github.com>
2023-03-27 19:19:58 -0400

    Update userWindow.java

--- src/coe528/group/project/userWindow.java
+++ src/coe528/group/project/userWindow.java
@@ -5,12 +5,18 @@
  */
 package coe528.group.project;
 
+import coe528.group.project.bookHandler.book;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -28,26 +34,54 @@ public class userWindow extends singletonWindow {
     VBox vbox;
     
     Button logoutButton;
-
-    User user;
+    Button buyButton;
+    Button redeemButton;
     
+    User user;
     
     private static userWindow instance;
     
+    private TableView<bookHandler> bookTable = new TableView<bookHandler>();
+    private ObservableList<bookHandler> books = FXCollections.observableArrayList(
+            new bookHandler(), new bookHandler(), new bookHandler());
+    
     private userWindow(){
         //set up all the ui bs here
-        welcome_l = new Text("perfectly safe for work test paragraph that certainly contains no bad words");
         
+        welcome_l = new Text("Welcome");
+
+        TableColumn nameCol = new TableColumn("Book Name");
+        nameCol.setMinWidth(450);
+        nameCol.setCellValueFactory(new PropertyValueFactory<bookHandler,String>("bookName"));
+
+        TableColumn priceCol = new TableColumn("Book Price");
+        priceCol.setMinWidth(200);
+        priceCol.setCellValueFactory(new PropertyValueFactory<bookHandler,String>("bookPrice"));
+
+        TableColumn selectCol = new TableColumn("Select");
+        selectCol.setMinWidth(100);
+        selectCol.setCellValueFactory(new PropertyValueFactory<bookHandler,String>("checkBox"));//change string
+
+        bookTable.setItems(books);
+        bookTable.getColumns().addAll(nameCol, priceCol, selectCol);
+
         logoutButton = new Button();
         logoutButton.setText("Log Out");
         logoutButton.setOnAction(new EventHandler<ActionEvent>() {
-            
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Logged out");
                 loginWindow.getInstance().show(stage);
             }
         });
+        
+        buyButton = new Button();
+        buyButton.setText("Buy");
+        
+        
+        redeemButton = new Button();
+        redeemButton.setText("Redeem points and Buy");
+        
         
         vbox = new VBox();
         
@@ -57,14 +91,17 @@ public class userWindow extends singletonWindow {
         vbox.setSpacing(16);
         
         vbox.getChildren().add(welcome_l);
+        vbox.getChildren().add(bookTable);
+        vbox.getChildren().add(buyButton);
+        vbox.getChildren().add(redeemButton);
         vbox.getChildren().add(logoutButton);
-       
+
+
         window = new StackPane();       
         window.getChildren().add(vbox);
         
         scene = new Scene(window, 800, 600);
-        
-            
+                    
     }
     
     public void show(Stage p){
