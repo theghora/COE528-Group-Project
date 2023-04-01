3cb31c91410a578483c2ad402c56b65c839d9962
sarah-fj <127571213+sarah-fj@users.noreply.github.com>
2023-04-01 00:37:40 -0400

    being problamatic

--- src/coe528/group/project/loginHandler.java
+++ src/coe528/group/project/loginHandler.java
@@ -1,60 +1,47 @@
 package coe528.group.project;
 
-import javafx.event.EventHandler;
-import javafx.stage.WindowEvent;
+import javafx.stage.Stage;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.Scanner;
 
-public class LoginHandler implements EventHandler<WindowEvent> {
+public class loginHandler {
 
-    @Override
-    public void handle(WindowEvent e) {
-        try {
-            // assuming the method loadData() is defined in the same class
-            loadData();
-        } catch (FileNotFoundException ex) {
-            System.out.println("" + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("" + ex.getMessage());
+    private static final String CUSTOMERS_FILE_PATH = "customers.txt";
+
+    public static boolean enter(String username, String password, Stage p){
+
+        if(username.equals("admin") && password.equals("admin")){
+            adminWindow.getInstance().show(p);
+            return true;
         }
 
-        // assuming observableList_customers is defined somewhere in the class
-        boolean customerExists = false;
-        for (Customer c : observableList_customers) {
-            // assuming the variables person, getUsername(), and getPassword() are defined in the Customer class
-            if (c.getUsername().equals(person.getUsername()) && c.getPassword().equals(person.getPassword())) {
-                customerExists = true;
-                break;
-            }
+        if(isCustomer(username, password)){
+            userWindow.getInstance().show(p);
+            return true;
         }
 
-        if (customerExists) {
-            System.out.println("Customer exists.");
-            // handle successful login
-        } else {
-            System.out.println("Customer does not exist.");
-            // display error message
-        }
+        return false;
     }
 
     private static boolean isCustomer(String username, String password){
         try {
             File customersFile = new File(CUSTOMERS_FILE_PATH);
-            try (Scanner scanner = new Scanner(customersFile)) {
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    String[] fields = line.split(",");
-                    if(fields.length == 3 && fields[1].equals(username) && fields[2].equals(password)){
-                        scanner.close();
-                        return true;
-                    }
+            Scanner scanner = new Scanner(customersFile);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] fields = line.split(",");
+                if(fields.length == 3 && fields[1].equals(username) && fields[2].equals(password)){
+                    scanner.close();
+                    return true;
                 }
             }
+            scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            return false;
-           
         }
+
         return false;
     }
 }
+
